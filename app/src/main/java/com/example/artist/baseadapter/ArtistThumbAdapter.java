372e3a22ac1653327d8e530e729972806040a277
Phuong Anh <tu.phuong.anh@mqsolutions.comnv>
2021-02-26 15:03:14 +0700

    Change Adapter for Home_ Artist

--- app/src/main/java/com/example/artist/baseadapter/ArtistThumbAdapter.java
+++ app/src/main/java/com/example/artist/baseadapter/ArtistThumbAdapter.java
@@ -11,66 +11,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.artist.model.ArtistData;
 import com.example.artist.databinding.ImageThumbnailBinding;
+import com.example.artist.model.BaseModelList;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArtistThumbAdapter extends RecyclerView.Adapter<ArtistThumbAdapter.MyViewHolder> {
-
-    ImageThumbnailBinding binding;
-    private List<ArtistData> artistDataList = new ArrayList<>();
-
-    public ArtistThumbAdapter() {
-    }
-
-    public void addData(List<ArtistData> list) {
-        artistDataList.addAll(list);
-        notifyDataSetChanged();
-    }
-
-    @NonNull
-    @Override
-    public ArtistThumbAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        binding = ImageThumbnailBinding.inflate(inflater, parent, false);
-        return new MyViewHolder(binding);
-
-    }
+public class ArtistThumbAdapter extends BaseThumbAdapter {
 
     @Override
-    public void onBindViewHolder(@NonNull ArtistThumbAdapter.MyViewHolder holder, int position) {
-        holder.bindView(artistDataList.get(position));
-    }
+    protected void bindData (ThumbnailViewHolder holder, BaseModelList model){
+        ArtistData artistData = (ArtistData) model;
 
-    @Override
-    public int getItemCount() {
-        return artistDataList.size();
-    }
-
-    class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(@NonNull ImageThumbnailBinding binding) {
-            super(binding.getRoot());
-        }
-
-            public void bindView(ArtistData artistData){
-                Context context = binding.getRoot().getContext();
+        Context context = thumbnailBinding.getRoot().getContext();
                 if (artistData.images != null && artistData.images.size() > 0) {
-                    String imageUrl = getImageUrl(artistData.images.get(0));
+                    String imageUrl = holder.getImageUrl(artistData.images.get(0));
                     Log.d("xxx", "imageUrl: " + imageUrl);
                     Glide.with(context)
                             .load(imageUrl)
-                            .into(binding.myImage);
+                            .into(thumbnailBinding.myImage);
                 }
-                binding.name.setText(artistData.name);
-                binding.text3.setText(artistData.country.name);
-                binding.text2.setText(artistData.genres);
-
-            }
-        }
-
-        public String getImageUrl(String relativeUrl) {
-            return "https://file.thedarkmetal.com/" + relativeUrl;
-        }
+        thumbnailBinding.name.setText(artistData.name);
+        thumbnailBinding.text3.setText(artistData.country.name);
+        thumbnailBinding.text2.setText(artistData.genres);
     }
+}
+
 
 
--- app/src/main/java/com/example/artist/baseadapter/ThumbnailViewHolder.java
+++ app/src/main/java/com/example/artist/baseadapter/ThumbnailViewHolder.java
@@ -1,16 +1,10 @@
 package com.example.artist.baseadapter;
 
-import android.content.Context;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.artist.databinding.ImageThumbnailBinding;
-import com.example.artist.model.BaseModelList;
-
-import java.util.List;
 
 public class ThumbnailViewHolder extends RecyclerView.ViewHolder {
 
